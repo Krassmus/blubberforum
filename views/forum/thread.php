e1d493bb5444815addaa9e2c41739d809bb0687a
Rasmus Fuhse <Krassmus@gmail.com>
2012-08-22 08:53:58 +0200

    redesign permalink

--- views/forum/thread.php
+++ views/forum/thread.php
@@ -34,14 +34,13 @@ $last_visit = object_get_visit($_SESSION['SessionSeminar'], "forum");
     </div>
     <div class="content_column">
         <div class="timer">
-            <span class="time" data-timestamp="<?= (int) $thread['mkdate'] ?>">
-                <?= (date("j.n.Y", $thread['mkdate']) == date("j.n.Y")) ? sprintf(_("%s Uhr"), date("G:i", $thread['mkdate'])) : date("j.n.Y", $thread['mkdate']) ?>
-            </span>
-            <a href="<?= $controller->url_for('forum/thread/' . $thread->getId(), array('cid' => $course_id)) ?>">
-                <?= Assets::img('icons/16/grey/group', array('class' => 'text-top', 'title' => _('Permalink'))) ?>
+            <a href="<?= $controller->url_for('forum/thread/' . $thread->getId(), array('cid' => $course_id)) ?>" class="permalink" title="<?= _("Permalink") ?>" style="background-image: url('<?= Assets::image_path("icons/16/grey/group") ?>');">
+                <span class="time" data-timestamp="<?= (int) $thread['mkdate'] ?>">
+                    <?= (date("j.n.Y", $thread['mkdate']) == date("j.n.Y")) ? sprintf(_("%s Uhr"), date("G:i", $thread['mkdate'])) : date("j.n.Y", $thread['mkdate']) ?>
+                </span>
             </a>
             <? if ($GLOBALS['perm']->have_studip_perm("tutor", $_SESSION['SessionSeminar']) or ($thread['user_id'] === $GLOBALS['user']->id)) : ?>
-            <a href="#" class="edit" title="<?= _("Bearbeiten") ?>" onClick="return false;" style="vertical-align: middle; opacity: 0.6; width: 14px; height:14px; display: inline-block; background: url('<?= Assets::image_path("icons/16/grey/tools.png") ?>') center center; background-position: center center;"></a>
+            <a href="#" class="edit icon" title="<?= _("Bearbeiten") ?>" onClick="return false;" style="background-image: url('<?= Assets::image_path("icons/16/grey/tools") ?>');"></a>
             <? endif ?>
         </div>
         <div class="name">
